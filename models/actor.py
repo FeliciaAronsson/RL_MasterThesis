0c972c1b670d70493b075bdcf59c07dcae469855
jelenanaas <jelena.naas@hotmail.com>
2026-02-10 10:17:05 +0100

    saved training

--- models/actor.py
+++ models/actor.py
@@ -55,3 +55,21 @@ class DDPGAgent:
         soft_update(self.target_critic, self.critic, self.tau)
 
 
+import random
+import numpy as np
+
+class ReplayBuffer:
+    def __init__(self, size=1_000_000):
+        self.buffer = []
+        self.size = size
+
+
+    def add(self, s, a, r, s2, d):
+        if len(self.buffer) >= self.size:
+            self.buffer.pop(0)
+        self.buffer.append((s, a, r, s2, d))
+
+
+    def sample(self, batch):
+        samples = random.sample(self.buffer, batch)
+        return map(np.array, zip(*samples))
